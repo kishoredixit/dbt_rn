9d643fcd5b76a80dbbb3e29281cb7c2a412ed7c6
Lakshmi Bala <lakshmi.bala@forecastear.com>
2022-07-26 14:30:09 +0530

    stg_recp_sample_model issue fixed

--- dbt_rn/macros/stg_recp_sample_model.sql
+++ dbt_rn/macros/stg_recp_sample_model.sql
@@ -2,6 +2,7 @@
 {%- set query -%}
 select column_name from dynamic_config.custom_config where table_name = '{{from_table}}' 
 {%- endset -%}
+{{ print("Running query: " ~ query) }}
 {%- set results = run_query(query) -%}
 {%- if execute -%}
 {%- set results_list = results.columns[0].values() -%}
@@ -12,14 +13,14 @@ with sample as (
 select
 {% for col in results_list %}
 {%- if not loop.last -%}
-"{{col}}" as "{{col}}".lower(),
+"{{col}}" as "{{col |lower}}" ,
 {%- endif -%}
 {%- if loop.last -%}
-"{{col}}" as "{{col}}".lower()
+"{{col}}" as "{{col |lower}}"
 {%- endif -%}
 {%- endfor -%}
 from 
-"Staging"."{{from_table}}"
+"Staging"."stg_{{from_table}}"
 ),
 final as (
     select * from sample
